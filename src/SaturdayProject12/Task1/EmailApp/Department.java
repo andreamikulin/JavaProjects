f243a8615b3316f1bcb5b4a4f3cd6c070f218d5c
Andrea Mikulin <andreamikulin@gmail.com>
2022-10-31 20:21:43 -0600

    Java Project Email App 10/29

---
+++ src/SaturdayProject12/Task1/EmailApp/Department.java
@@ -0,0 +1,6 @@
+package SaturdayProject12.Task1.EmailApp;
+
+public enum Department {
+    SALES, DEVELOPMENT, ACCOUNTING;
+
+}
---
+++ src/SaturdayProject12/Task2/Course.java
@@ -0,0 +1,5 @@
+package SaturdayProject12.Task2;
+
+public enum Course {
+    HISTORY_101, MATHEMATICS_101, CHEMISTRY_101, COMPUTER_SCIENCE_100;
+}
